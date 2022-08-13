2a9412c164dfb7453699c78a251db752b6f0334a
muhammet aksoy <muhammetaksoy176@gmail.com>
2022-08-14 00:24:59 +0300

    css düzenlemeleri yapıldı

--- src/app/shared/module/angularmaterial/angularmaterial.module.ts
+++ src/app/shared/module/angularmaterial/angularmaterial.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 
 
@@ -8,7 +12,11 @@ import {MatIconModule} from '@angular/material/icon';
   declarations: [],
   exports: [
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatSidenavModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatCardModule
   ]
 })
 export class AngularmaterialModule { }
